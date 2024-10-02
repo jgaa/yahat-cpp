9049f050258849d41f732d5d8547b7f468743153
Jarle Aase <jarle@jgaa.com>
2024-10-02 15:37:53 +0300

    Detection of existing logfault target in cmake

--- cmake/3rdparty.cmake
+++ cmake/3rdparty.cmake
@@ -9,29 +9,33 @@ link_directories(${EXTERNAL_PROJECTS_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR})
 include_directories($<BUILD_INTERFACE:${EXTERNAL_PROJECTS_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}>)
 
 if (USE_LOGFAULT)
-    if (LOGFAULT_ROOT)
-        # Assume that we are using a library that might not be available yet
-        message("LOGFAULT_ROOT: ${LOGFAULT_ROOT}")
-        set(LOGFAULT_DIR ${LOGFAULT_ROOT})
-        include_directories(${LOGFAULT_DIR})
+    if (TARGET logfault)
+        message("Using existing logfault target")
     else()
-        find_path(LOGFAULT_DIR NAMES logfault.h PATH_SUFFIXES logfault)
-    endif()
-    message("LOGFAULT_DIR: " ${LOGFAULT_DIR})
-    if (NOT LOGFAULT_DIR STREQUAL "LOGFAULT_DIR-NOTFOUND" )
-        message ("Using existing logfault at: ${LOGFAULT_DIR}")
-        add_library(logfault INTERFACE IMPORTED)
-    else()
-        message ("Embedding logfault header only library")
-        ExternalProject_Add(logfault
-            PREFIX "${EXTERNAL_PROJECTS_PREFIX}"
-            GIT_REPOSITORY "https://github.com/jgaa/logfault.git"
-            GIT_TAG "master"
-            CMAKE_ARGS
-                -DCMAKE_INSTALL_PREFIX=${EXTERNAL_PROJECTS_INSTALL_PREFIX}
-                -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-                -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-                -DCMAKE_BUILD_TYPE='${CMAKE_BUILD_TYPE}'
-        )
-    endif() # embed
+        if(LOGFAULT_ROOT)
+            # Assume that we are using a library that might not be available yet
+            message("LOGFAULT_ROOT: ${LOGFAULT_ROOT}")
+            set(LOGFAULT_DIR ${LOGFAULT_ROOT})
+            include_directories(${LOGFAULT_DIR})
+        else()
+            find_path(LOGFAULT_DIR NAMES logfault.h PATH_SUFFIXES logfault)
+        endif()
+        message("LOGFAULT_DIR: " ${LOGFAULT_DIR})
+        if (NOT LOGFAULT_DIR STREQUAL "LOGFAULT_DIR-NOTFOUND" )
+            message ("Using existing logfault at: ${LOGFAULT_DIR}")
+            add_library(logfault INTERFACE IMPORTED)
+        else()
+            message ("Embedding logfault header only library")
+            ExternalProject_Add(logfault
+                PREFIX "${EXTERNAL_PROJECTS_PREFIX}"
+                GIT_REPOSITORY "https://github.com/jgaa/logfault.git"
+                GIT_TAG "master"
+                CMAKE_ARGS
+                    -DCMAKE_INSTALL_PREFIX=${EXTERNAL_PROJECTS_INSTALL_PREFIX}
+                    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+                    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+                    -DCMAKE_BUILD_TYPE='${CMAKE_BUILD_TYPE}'
+            )
+        endif() # embed
+    endif() # existing target
 endif() # use logfault
